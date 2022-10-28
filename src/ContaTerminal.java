add8d55bfaa315762a9817cb0d483ae1bf6d4128
Bruno Dorea <brunodorea@outlook.com.br>
2022-10-28 13:15:27 -0300

    Initial commit

---
+++ src/ContaTerminal.java
@@ -0,0 +1,12 @@
+public class ContaTerminal {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Hello, World!");
+        //TODO: conhecer e importar a classe scanner
+
+        // exibir as mensagens para o nosso usuario
+
+        // obter pela scanner os valores digitados no terminal
+
+        // exibir a mensagem de conta criada
+    }
+}
